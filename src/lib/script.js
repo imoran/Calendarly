92c58e4a6654bfa8aabddfe3942c31ff3d37dd79
Isis Moran <imoran@mail.ccsf.edu>
2018-04-17 18:44:57 -0700

    Finish header

--- src/lib/script.js
+++ src/lib/script.js
@@ -6,20 +6,28 @@ splashHeader.classList.add('splash-header');
 
 function createHeader() {
   const splashText = document.createElement('h1');
+  const div = document.createElement('div');
+  const triangle = document.createElement('div');
   splashText.append('Calendarly');
-  splashHeader.appendChild(splashText);
+  triangle.classList.add('triangle');
+  div.append(splashText);
+  div.append(triangle);
+  splashHeader.appendChild(div);
   return splashHeader;
 }
 
 function createSplash() {
   const div = document.createElement('div');
+  const h2 = document.createElement('h2');
   div.classList.add('splash-picture-button');
   const img = document.createElement('img');
   const button = document.createElement('button');
   button.append('Book Appointment Now');
+  h2.append('Schedule Your Appointments Quicker!');
   img.setAttribute('src', 'https://images.pexels.com/photos/434337/pexels-photo-434337.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
   img.classList.add('splash-picture');
   div.appendChild(img);
+  div.appendChild(h2);
   div.appendChild(button);
   const header = createHeader();
   header.append(div);
