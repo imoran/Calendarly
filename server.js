d73e86883f7a51180e95f2e460475636d8aef612
Isis Moran <imoran@mail.ccsf.edu>
2018-04-20 10:36:27 -0700

    Fix index.html

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
