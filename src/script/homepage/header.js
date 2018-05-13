3cf186ae4ac3b21819c61514acabd7af6372a9bb
Isis Moran <imoran@mail.ccsf.edu>
2018-05-13 13:47:50 -0700

    Fix broken links

--- src/script/homepage/header.js
+++ src/script/homepage/header.js
@@ -2,17 +2,23 @@ const navBar = () => {
 	const img = document.createElement('img');
 	const ul = document.createElement('ul');
 	const list = ['About', 'Features', 'Contact Us'];
+	const links = ['https://github.com/imoran/Calendarly',
+								 'https://github.com/imoran/Calendarly',
+								 'mailto:isismoran93@gmail.com'];
 	const imgWrapper = document.createElement('div');
 	imgWrapper.setAttribute('id', 'img-wrapper');
 	ul.classList.add('nav-bar');
 	ul.setAttribute('id', 'top-nav');
 	img.setAttribute('src', 'src/image/hamburger.png');
 
-	list.forEach(l => {
+	for (let i = 0; i < 3; i++) {
 		const li = document.createElement('li');
-		li.append(l);
-		ul.appendChild(li);
-	});
+		const a = document.createElement('a');
+		a.textContent = list[i];
+		a.href = links[i];
+		li.append(a);
+		ul.append(li);
+	}
 
 	imgWrapper.onclick = function() {
 		let navbar = document.getElementById('top-nav');
